00bf6d529788f3cce22e0ea62bd99fcf60ffede8
Rasil7 <rasilaziz99@gmail.com>
2025-07-14 05:51:15 +0300

    Create  firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBzrkb6xgcyF5aVv3MV3vwJSwieIgyj-ao",
+  authDomain: "tasklistweb-43653.firebaseapp.com",
+  projectId: "tasklistweb-43653",
+  storageBucket: "tasklistweb-43653.firebasestorage.app",
+  messagingSenderId: "875266511120",
+  appId: "1:875266511120:web:5df1cf617ebd7784a79595",
+  measurementId: "G-H694DB31BF"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export { app, analytics };
